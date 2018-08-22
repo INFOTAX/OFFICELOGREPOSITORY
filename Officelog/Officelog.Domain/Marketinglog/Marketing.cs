38e9ad72925eb5daff6f4537d28cca9e212946a6
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-08-22 18:12:48 +0530

    added Domain

---
+++ Officelog/Officelog.Domain/Marketinglog/Marketing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Officelog.Domain.Marketinglog
+{
+    class Marketing
+    {
+        public int Id { get; set; }
+        public string TradeName { get; set; }
+        public int ContactNumber { get; set; }
+        
+
+    }
+}
