3e411b1df5f26337660b8d55c200bb1f510e6f8e
Vishal Rathore <vishalrathor9211@gmail.com>
2018-08-28 12:06:56 +0530

    done up to repository base

--- Officelog/Officelog.Domain/Companylog/Company.cs
+++ Officelog/Officelog.Domain/Companylog/Company.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Officelog.Domain.Companylog
 {
-    class Company
+    public class Company
     {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public int ContactNumber { get; set; }
+        public string QueryHandling { get; set; }
+        public string ServiceProvided { get; set; }
+
+        public string VisitorType {get; set;}
+
+        public bool SoftwareInterested { get; set; }
+        public string RateUs { get; set; }
+
+        public string SuggestionForYes { get; set; }
+        public string SuggestionForNo { get; set; }
     }
 }
