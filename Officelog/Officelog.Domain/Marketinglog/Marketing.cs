b57ac0290f7fabfffdb4157360f6beabc9443619
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-08-28 12:08:43 +0530

    Added marketing log

--- Officelog/Officelog.Domain/Marketinglog/Marketing.cs
+++ Officelog/Officelog.Domain/Marketinglog/Marketing.cs
@@ -4,12 +4,26 @@ using System.Text;
 
 namespace Officelog.Domain.Marketinglog
 {
-    class Marketing
+    public class Marketing
     {
         public int Id { get; set; }
-        public string TradeName { get; set; }
+        public string Name { get; set; }
         public int ContactNumber { get; set; }
-        
+        public bool ServiceInterested { get; set; }
+        public bool SoftwareInterested { get; set; }
+        public ConversionStatus ConversionStatus { get; set; }
+        public string RateUs { get; set; }
+        public string SuggestionForYes { get; set; }
+        public string SuggestionForNo { get; set; }
+        public string ServiceType { get; set; }
+        public double Rate { get; set; }
+        public string  Area { get; set; }
+
+    }
+
+    public enum ConversionStatus{
+        Achieved = 1,
+        NotAchieved = 2
 
     }
 }
