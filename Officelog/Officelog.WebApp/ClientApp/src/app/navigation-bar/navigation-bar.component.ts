8ce2fb091aa3863d051029fe803c6a7844062e6f
Ankitacse <ankitastar92@gmail.com>
2018-08-29 15:25:27 +0530

    editdashboard

---
+++ Officelog/Officelog.WebApp/ClientApp/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.css']
+})
+export class NavigationBarComponent implements OnInit {
+  [x: string]: any;
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.items = [ 
+      {label: 'Home', icon: 'fa fa-fw fa-home' ,routerLink: ['/dashboard_log']},
+    {label: 'Company list', icon: 'fa fa-fw fa-book' ,routerLink: ['/company_log_list']},
+  {label: 'Marketing list', icon: 'fa fa-fw fa-book',routerLink: ['/marketing_log_list']}
+]
+
+  }
+  marketingLogList() {
+
+    this._router.navigate(['marketing_log_list']);
+    // this.compLog=true;
+    // this.markLog=false;
+  }
+  companyLogList(){
+
+    this._router.navigate(['company_log_list']);
+    // this.compLog=true;
+    // this.markLog=false;
+  }
+}
