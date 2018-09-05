11b9d7d3c6a64b62b0698c421d2df3d626976a0c
Ajit bharti <a.bharti0310@gmail.com>
2018-09-05 14:01:53 +0530

    pai-chart

--- Officelog/Officelog.WebApp/ClientApp/src/app/marketing-dashboard/marketing-dashboard.component.ts
+++ Officelog/Officelog.WebApp/ClientApp/src/app/marketing-dashboard/marketing-dashboard.component.ts
@@ -50,39 +50,7 @@ export class MarketingDashboardComponent implements OnInit {
     this.marketingAndConversionService.getConversionReports().subscribe(res => {
       this.conversionReport = res;
       this.x=this.conversionReport.totalConversions;
-<<<<<<< HEAD
-      console.log(this.x,this.y,this.z);
-      this.conversionReport = { 
-        labels: ['Total Conversions','Total Software Interested','Total Service Interested'],
-        datasets: [
-            { 
-                data: [this.x,this.y,this.z],
-                backgroundColor: [
-                    "#FF6384",
-                    "#36A2EB",
-                    "#FFCE56"
-                ],
-                hoverBackgroundColor: [
-                    "#FF6384",
-                    "#36A2EB",
-                    "#FFCE56"
-                ]
-            }]    
-        }
-        this.options={
-        
-                legend: {
-                  labels:{
-                      fontSize: 18,
-                      fontColor: 'black',
-                      padding: 20,
-                  },
-                    position: 'left',
-                }}
-      
-=======
       this.getPieChartForConversion(this.conversionReport);
->>>>>>> c11266b342cc4e445a2489e535d13ecd560e9c2f
       console.log(this.x);
       
       (error : any) => {
